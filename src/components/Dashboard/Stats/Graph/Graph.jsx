85bdebfdbda055e22ce9c5f426696999ca8760a4
nicolefab <n.fabianparodi@gmail.com>
2022-02-20 12:33:00 -0300

    graph

--- src/components/Dashboard/Stats/Graph/Graph.jsx
+++ src/components/Dashboard/Stats/Graph/Graph.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-/* import Chart from "react-apexcharts"; */
+import Chart from "react-apexcharts";
 
-const Graph = () => {
-  /*   const [data, setData] = useState({
+const Graph = (props) => {
+  /* const [data, setData] = useState({
     series: [
       {
-        data: [completed, incomplete],
+        data: [parametro, contador],
       },
     ],
     options: {
@@ -23,31 +23,34 @@ const Graph = () => {
         enabled: false,
       },
       xaxis: {
-        categories: ["Completed", "Incomplete"],
+        categories: [parametro, enviosTotales],
       },
     },
   }); */
-
-  /*   useEffect(() => {
+  /* useEffect(() => {
     setData({
       ...data,
       series: [
         {
-          data: [completed, incomplete],
+          data: [props.data, props.enviosTotales],
         },
       ],
     });
-  }, [completed, incomplete]); */
-
+  }, [,]);
+ */
   return (
     <div className="border p-3">
       <h3>Graph</h3>
-      {/* <Chart
-        options={data.options}
-        series={data.series}
-        type="bar"
-        height={350}
-      /> */}
+      {/* {props.datos.length > 0 ? (
+        <Chart
+          options={data.options}
+          series={data.series}
+          type="bar"
+          height={350}
+        />
+      ) : (
+        <p>Cargando...</p>
+      )} */}
     </div>
   );
 };
